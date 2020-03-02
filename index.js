6a33c380499342983355269637412426e1b04679
wilsondonizetti <wilsondonizetti@gmail.com>
2020-03-02 00:22:17 -0300

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
+import Speech from './Speech';
 import './style.css';
 
 class App extends Component {
@@ -18,6 +19,7 @@ class App extends Component {
         <p>
           Start editing to see some magic happen :)
         </p>
+        <Speech />
       </div>
     );
   }
