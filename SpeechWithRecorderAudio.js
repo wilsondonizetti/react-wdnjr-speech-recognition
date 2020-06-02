c9c517c93b0d80b003bd3df57a1792c20596874f
wilsondonizetti <wilsondonizetti@gmail.com>
2020-06-02 11:31:18 -0300

    1 file modified

--- SpeechWithRecorderAudio.js
+++ SpeechWithRecorderAudio.js
@@ -1,21 +1,36 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const SpeechWithRecorderAudio = (props) => {
   //let mediaRecorder;
   let shouldStop = false;
   let stopped = false;  
+  const [mediaRecorder, setMediaRecorder] = useState(); 
+  const [recordedChunks, setRecordedChunks] = useState([]);
+
  const handleSuccess = (stream) => {  
     console.log(stream);
+    
+    
+    const options = { 
+      mimeType: 'audio/webm',
+      audioBitsPerSecond : 44100,
+      bitsPerSecond: 8,
+    };
+    stream = new MediaRecorder(stream, options);
+    //stream.setAudioSource(MediaRecorder.AudioSource.MIC);
+    //stream.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+    //stream.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+    //stream.setAudioEncodingBitRate(16);
+    //stream.setAudioSamplingRate(44100);
 
-    const options = {mimeType: 'video/webm;codecs=vp9'};
-    const recordedChunks = [];
-    const mediaRecorder = new MediaRecorder(stream, options);  
+    console.log('output',mediaRecorder);
 
-    mediaRecorder.ondataavailable = e => {
-      console.log('DATA', e);
+    stream.ondataavailable = (e) => {
+      console.log('DATA', e.data);
       if (e.data.size > 0) {
         recordedChunks.push(e.data);
       }
+      setRecordedChunks(recordedChunks);
 
       if(shouldStop === true && stopped === false) {
         mediaRecorder.stop();
@@ -23,15 +38,35 @@ const SpeechWithRecorderAudio = (props) => {
       }
     };
 
-    mediaRecorder.onstop = (e) => {
+    stream.onstop = (e) => {
       // downloadLink.href = URL.createObjectURL(new Blob(recordedChunks));
       // downloadLink.download = 'acetest.wav';
       console.log('stop', e);
+      const audioBlob = new Blob(recordedChunks);
+      console.log('audioBlob', audioBlob);
     };
 
-    mediaRecorder.start();
-    console.log(mediaRecorder.state);
+    stream.onresume = (e) => {
+      console.log('onresume', e);
+    };
+
+    stream.onstart = (e) => {
+      console.log('onstart', e);
+    };
+
+    stream.onpause = (e) => {
+      console.log('onpause', e);
+    };
+
+    stream.onerror = (e) => {
+      console.log('onerror', e);
+    };
+
+    setMediaRecorder(stream);
+
+    
   };
+
   useEffect(()=>{
     navigator.mediaDevices.getUserMedia({audio: true, video: false})
     .then(handleSuccess)
@@ -40,6 +75,25 @@ const SpeechWithRecorderAudio = (props) => {
     }); 
     console.log('mount');
   },[]);
+
+  useEffect(()=>{
+    console.log('eff', mediaRecorder);
+    if(mediaRecorder && mediaRecorder.state != 'recording'){
+      mediaRecorder.start(5000);
+      console.log(mediaRecorder.state);
+      setTimeout(event => {
+        console.log("stopping");
+        mediaRecorder.stop();
+      }, 9000);
+    }      
+  },[mediaRecorder]);
+
+  useEffect(()=>{
+    console.log('recordedChunks', recordedChunks);
+
+
+      
+  },[recordedChunks]);
 
   return (<div>Recorder Audio</div>);
 }
